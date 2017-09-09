7de4b99e2216124a26aa9ab422780898ec62ffae
xeon17 <31801969+xeon17@users.noreply.github.com>
2017-09-09 23:40:21 +0200

    Update Lobby.lua
    Added start method.

--- server/Lobby.lua
+++ server/Lobby.lua
@@ -9,6 +9,12 @@ Lobby.positions = {
 	{474.81, -14.98, 1003.7, 180}
 }
 
+function Lobby.start(minutes)
+    setTimer(function()
+	    EventManager.event:start()
+	end, 60000*minutes, 1)
+end
+	
 
 function Lobby.addPlayer(player)
 	table.insert(Lobby.players, player);
