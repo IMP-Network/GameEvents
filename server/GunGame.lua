8fe2e812ad68c48fa6aaae5ba34e6424651de212
Banex <mini-banex@hotmail.com>
2017-09-09 17:34:01 -0300

    Create GunGame.lua

---
+++ server/GunGame.lua
@@ -0,0 +1,15 @@
+GunGame = setmetatable({}, Effect)
+GunGame.__index = GunGame
+GunGame.__parent = Effect
+
+function GunGame:parseArgs(map, mins, value, stats) 
+	if(not map or not mins) then
+		self.argUsage = "<mapa> <minutos> <valor> <stats>"
+		return false
+	end	
+	self.settings.map = tonumber(map) or 0
+	self.settings.mins = tonumber(mins) or 5
+	self.settings.value = tonumber(value) or 100000
+	self.settings.stats = tonumber(stats) or 999
+	return true
+end
