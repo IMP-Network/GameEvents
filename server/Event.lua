49a90d51a4369c1573c1d96f813c670763f55e31
xeon17 <31801969+xeon17@users.noreply.github.com>
2017-09-09 19:44:33 +0200

    Create Event.lua

---
+++ server/Event.lua
@@ -0,0 +1,21 @@
+local super = Class("GameEvent", LuaObject).getSuperclass()
+GameEvents.players = ArrayList();
+
+function GameEvent:init()
+    super.init(self)
+    
+    print("GameEvents: init")
+    
+    self.eventHandler = function(player, commandName, eventType, map)
+        if (eventType == 1) then
+            GunGame(map)
+        end
+    end
+    addCommandHandler("event", self.eventHandler)
+    
+    return self
+end
+
+function GameEvent.onPlayerEnter(event, player)
+      GameEvent.players:add(player)
+end
