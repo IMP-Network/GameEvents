e6fb25f31f607dae5a623104f4db0f4c0f87839d
Banex <mini-banex@hotmail.com>
2017-09-09 18:50:54 -0300

    Update GunGame.lua

--- server/GunGame.lua
+++ server/GunGame.lua
@@ -14,3 +14,22 @@ function GunGame:parseArgs(map, mins, value, stats)
 	return true
 end
 
+function GunGame:onCreate()
+	self.lobby = Lobby.getInstance()
+	self.scoreHud = ScoreHud.getInstance()
+	self.mapLoader = MapLoader.getInstance()
+
+	self.weaponList = {22,23,24,25,26,27,28,29,32,30,31,33,34,35,36,37,38}
+
+	if (self.settings) then
+		self.map = self.mapLoader:load("deathmatch", self.settings.map)
+		if (self.map) then
+			root:outputChat(("[GUN-GAME] #00FF00 Evento em '%s' criado digite /participar para participar"):format(self.map.name), 255, 100, 100, true)	
+			self.lobby:start(1000 * 60)
+			outputDebugString("GunGame:onCreate")
+			return true
+		end
+		outputDebugString("GunGame:invalidMap!")
+	end
+	return false
+end
