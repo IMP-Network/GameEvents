730995ea2b851d727eb362f81c254e65730eeb80
xeon17 <31801969+xeon17@users.noreply.github.com>
2017-09-09 22:48:05 +0200

    Update Lobby.lua
    Fixed notepad.

--- server/Lobby.lua
+++ server/Lobby.lua
@@ -4,17 +4,17 @@ Lobby.players = {}
 
 Lobby.positions = {
     {487.33, -4.62, 1002.08, 180},
-	  {484.98, -22.85, 1003.11, 360},
-	  {474.81, -14.98, 1003.7, 180}
+	{484.98, -22.85, 1003.11, 360},
+	{474.81, -14.98, 1003.7, 180}
 }
 
 
 function Lobby.addPlayer(player)
-     table.insert(Lobby.players, player);
-  
-	   local posX, posY, posZ, posRot = unpack(Lobby.positions)
-	   player.position = Vector3(posX, posY, posZ)
-	   player.rotation = Vector3(0, 0, posRot)
+    table.insert(Lobby.players, player);
+	
+	local posX, posY, posZ, posRot = unpack(Lobby.positions)
+	player.position = Vector3(posX, posY, posZ)
+	player.rotation = Vector3(0, 0, posRot)
 end
 
 function Lobby.isInside(element)
