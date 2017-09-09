43e2f26c873e9f48f435b01cb4a2e868e4e0762c
Banex <mini-banex@hotmail.com>
2017-09-09 17:50:07 -0300

    Update Lobby.lua

--- server/Lobby.lua
+++ server/Lobby.lua
@@ -3,22 +3,22 @@ Lobby.__index = Lobby
 Lobby.players = {}
 
 Lobby.positions = {
-    {487.33, -4.62, 1002.08, 180},
+	{487.33, -4.62, 1002.08, 180},
 	{484.98, -22.85, 1003.11, 360},
 	{474.81, -14.98, 1003.7, 180}
 }
 
 
 function Lobby.addPlayer(player)
-    table.insert(Lobby.players, player);
-	
+	table.insert(Lobby.players, player);
+
 	local posX, posY, posZ, posRot = unpack(Lobby.positions)
 	player.position = Vector3(posX, posY, posZ)
 	player.rotation = Vector3(0, 0, posRot)
 end
 
 function Lobby.isInside(element)
-    for index,player in pairs(Lobby.players)
+	for index,player in pairs(Lobby.players)
 	    if(plyer == element) then
 		    return true
 		end
