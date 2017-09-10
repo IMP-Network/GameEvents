6b1bd996cb6384fb2d838d333d820ec61d6702a7
Banex <mini-banex@hotmail.com>
2017-09-09 22:31:16 -0300

    Update GunGame.lua

--- server/GunGame.lua
+++ server/GunGame.lua
@@ -1,12 +1,20 @@
 GunGame = setmetatable({}, EventManager)
-GunGame.__index = GunGame
 GunGame.__parent = EventManager
+GunGame.instance = nil
+
+function GunGame.getInstance()
+	if(not GunGame.instance) then
+		GunGame.instance = setmetatable({}, GunGame)
+	end
+	return GunGame.instance
+end
 
 function GunGame:parseArgs(map, mins, value, stats) 
 	if(not map or not mins) then
 		self.argUsage = "<mapa> <minutos> <valor> <stats>"
 		return false
-	end	
+	end
+	self.settings = {}
 	self.settings.map = tonumber(map) or 0
 	self.settings.mins = tonumber(mins) or 5
 	self.settings.value = tonumber(value) or 100000
@@ -16,9 +24,8 @@ end
 
 function GunGame:onCreate()
 	self.lobby = Lobby.getInstance()
-	self.scoreHud = ScoreHud.getInstance()
+	self.eventHud = eventHud.getInstance()
 	self.mapLoader = MapLoader.getInstance()
-
 	self.weaponList = {22,23,24,25,26,27,28,29,32,30,31,33,34,35,36,37,38}
 
 	if (self.settings) then
@@ -42,29 +49,74 @@ function GunGame:onPlayerEnter(player)
 		return player:outputChat("[GUN-GAME] #00FF00Você já está no evento...", 255, 100, 100, true)
 	end
 
-	EventManager.onPlayerEnter(player)
+	EventManager:getInstance():onPlayerEnter(player)
 	self:spawnPlayer(player)
 	player:setData("event.killspree", 0, false)
 	player:setData("event.level", 1, false)
-	self.scoreHud:setMode(player, "Lobby")
+	self.eventHud:setMode(player, "Lobby")
 	player:outputChat("[GUN-GAME] #00FF00Você entrou no lobby, aguarde outros players...", 255, 100, 100, true)
 end
 
+function GunGame:onPlayerExit(player, reason)
+	player:removeData("event.killspree")
+	player:removeData("event.level")
+	EventManager:getInstance():onPlayerExit(player,reason)
+end
+
+function GunGame:onStart()
+	self.started = true	
+
+	local mins = self.settings.mins or 1
+	Timer(function()
+		EventManager:getInstance():finish() 
+	end, 1000 * 60 * mins, 1)
+
+	EventManager:getInstance():getPlayers():each(function(player)
+		self:spawnPlayer(player)
+		player:triggerEvent("playSound", resourceRoot, "event:prepare")
+	end)	
+end
+
+function GunGame:onFinish()
+	local winner = nil
+	local winnerPoints = 0
+	local value = self.settings.value
+	EventManager.getInstance():getPlayers():each(function(player)
+		local points = player:getData("event.points") or 0
+		if(points > winnerPoints) then
+			winnerPoints = points
+			winner = player
+		end
+	end)
+	if (winner and isElement(winner)) then
+		winner:giveMoney(winner, value)
+		root:outputChat("[GUN-GAME]#00FF00 O jogador %s venceu o evento com %s pts e ganhou $%s."):format(winner:getName(), winnerPoints, value), 255, 100, 100, true)
+	end	
+end
+
+function GunGame:onDestroy(reason)
+	self.lobby:dispose()
+	self.mapLoader:dispose()
+	self.eventHud:dispose()
+end
+
 function GunGame:spawnPlayer(player)
-	if (self.started) then
-		local spawn = self.map.spawns[math.random(#self.map.spawns)]
-		player:setPosition(spawn.posX, spawn.posY, spawn.posZ)
-		player:setRotation(0, 0, spawn.angleZ)
-		player:setInterior(player, spawn.interior)
-		player:setDimension(player, self.map.dimension)
-		player:setHealth(100)
-		player:setArmor(player, 100)
-		player:setMoney(player, 0)
-		giveWeapon(player,self.weaponList[player:getData("event.level")], 9999, true)
-	else
-		self.lobby:addPlayer(player)
+	if (not self.started) then
+		return self.lobby:addPlayer(player)
 	end
-	return false
+	local spawn = self.map.spawns[math.random(#self.map.spawns)]
+	player:setPosition(spawn.posX, spawn.posY, spawn.posZ)
+	player:setRotation(0, 0, spawn.angleZ)
+	player:setInterior(player, spawn.interior)
+	player:setDimension(player, self.map.dimension)
+	player:setHealth(100)
+	player:setArmor(player, 100)
+	player:setMoney(player, 0)
+	giveWeapon(player,self.weaponList[player:getData("event.level")], 9999, true)
+end
+
+function GunGame:onPlayerQuit(player, quitType, reason, responsibleElement)
+	EventManager:getInstance():removePlayer(player, "quit")
 end
 
 function GunGame:updatePlayerPoints(player,points)
@@ -78,7 +130,7 @@ function GunGame:onPlayerWasted(player, totalAmmo, killer, killerWeapon, bodypar
 		return
 	end
 	--It is necessary to recreate part of this function in the client, for better performance in future
-	if (player:getData("event") == self.source) then
+	if (player:getData("event")) then
 		if (isElement(killer) and player ~= killer) then
 			if (killer:getData("event")) then
 				local points = killer:getData(killer, "event.points") or 0
