a843fcf9ee7145a461041628cdb7903ff5d6f181
Banex <mini-banex@hotmail.com>
2017-09-10 09:43:26 -0300

    Update util.lua

--- shared/util.lua
+++ shared/util.lua
@@ -25,3 +25,92 @@ function table.each(t, index, callback, ...)
 	end
 	return t
 end
+
+function convertNumber(number)  
+    local formatted = number
+    while true do
+        formatted, k = string.gsub(formatted,"^(-?%d+)(%d%d%d)",'%1.%2')    
+        if (k == 0) then      
+            break   
+        end  
+    end  
+    return formatted
+end
+
+function table.find(t, ...)
+	local args = { ... }
+	if #args == 0 then
+		for k,v in pairs(t) do
+			if v then
+				return k
+			end
+		end
+		return false
+	end
+	
+	local value = table.remove(args)
+	if value == '[nil]' then
+		value = nil
+	end
+	for k,v in pairs(t) do
+		for i,index in ipairs(args) do
+			if type(index) == 'function' then
+				v = index(v)
+			else
+				if index == '[last]' then
+					index = #v
+				end
+				v = v[index]
+			end
+		end
+		if v == value then
+			return k
+		end
+	end
+	return false
+end
+
+function table.find(tableToSearch, index, value)
+	if not value then
+		value = index
+		index = false
+	elseif value == '[nil]' then
+		value = nil
+	end
+	for k,v in pairs(tableToSearch) do
+		if index then
+			if v[index] == value then
+				return k
+			end
+		elseif v == value then
+			return k
+		end
+	end
+	return false
+end
+
+function table.maptry(t, callback, ...)
+	for k,v in pairs(t) do
+		t[k] = callback(v, ...)
+		if not t[k] then
+			return false
+		end
+	end
+	return t
+end
+
+function string:split(sep)
+	if #self == 0 then
+		return {}
+	end
+	sep = sep or ' '
+	local result = {}
+	local from = 1
+	local to
+	repeat
+		to = self:find(sep, from, true) or (#self + 1)
+		result[#result+1] = self:sub(from, to - 1)
+		from = to + 1
+	until from == #self + 2
+	return result
+end
