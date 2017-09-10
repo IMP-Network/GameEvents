4916bd59d4d8a73573cdef838d6cc5c1e31188f0
Banex <mini-banex@hotmail.com>
2017-09-10 09:53:50 -0300

    Update MapLoader.lua

--- server/MapLoader.lua
+++ server/MapLoader.lua
@@ -1,1 +1,10 @@
+MapLoader = setmetatable({}, EventManager)
+MapLoader.__parent = EventManager
+MapLoader.instance = nil
 
+MapLoader.ATTRS = {
+	spawnpoint = { 'position', 'rotation', 'vehicle', 'paintjob', 'upgrades' },
+	checkpoint = { 'id', 'nextid', 'position', 'size', 'color', 'type', 'vehicle', 'paintjob', 'upgrades' },
+	object = { 'position', 'rotation', 'model' },
+	pickup = { 'posX', 'posY', 'posZ', 'id', 'type', 'vehicle', 'paintjob', 'upgrades', 'respawn' }
+}
