b96550bc297dbda4123b596e3a9090403a09aedf
Banex <mini-banex@hotmail.com>
2017-09-10 14:45:59 -0300

    Update MapLoader.lua

--- server/MapLoader.lua
+++ server/MapLoader.lua
@@ -13,44 +13,102 @@ end
 function MapLoader:init()
 	local xml = xmlLoadFile("meta.xml")
 	local childs = xmlNodeGetChildren(xml)
-	for k,i in ipairs (childs) do
-		if (xmlNodeGetName(i) == "maploader") then 
-			--local src = xmlNodeGetAttribute(i, "type"):gsub("/[^\\\/]*$/", '')
+	for i,node in ipairs (childs) do
+		if (xmlNodeGetName(node) == "maploader") then 
+			--local src = xmlNodeGetAttribute(node, "type"):gsub("/[^\\\/]*$/", '')
 			--[[local result = {}
-			local src = xmlNodeGetAttribute(i, "type"):sub(5):gsub("(%w+)", function(w)
+			local src = xmlNodeGetAttribute(node, "type"):sub(5):gsub("(%w+)", function(w)
 				table.insert(result, w)
 			end)--]]
-			local data = xmlNodeGetAttribute(i, "src"):split("(%w+)")
+			local data = xmlNodeGetAttribute(node, "src"):split("(%w+)")
 			self:register(data[2], data[3])
 		end
 	end
+	xmlUnloadFile(xml)
 	return self
+end
+
+function MapLoader:load(type,id)
+	local src = self:getByID(type,id)
+	if (not src) then
+		return outputDebugString("Map not registered")
+	end
+	local xml = xmlLoadFile(src)
+	if (not xml) then
+		return outputDebugString("Map not found")
+	end
+	
+	local map = setmetatable({}, self)
+	map.objects = {}
+	map.spawns = {}
+	map.checkpoints = {}
+	map.pickups = {}
+
+	for component, value in pairs(self:getMapData(xml)) do
+		if (component == "spawnpoint") then
+			table.insert(map.spawns, {})
+			for k, v in pairs(value) do
+				table.insert(map.spawns[#map.spawns], k, v)
+			end
+		elseif (component == "object") then
+			local obj = createObject(value.model, value.posX, value.posY, value.rotX, value.rotY, value.rotZ)
+			setObjectScale(obj, (value.scale or 1))
+			setElementAlpha(obj, (value.alpha or 255))
+			setElementCollisionsEnabled(obj, (value.collisions or false))
+			setElementDoubleSided(obj, (value.doubleSided or false))
+			setElementDimension(obj, (value.dimension or 0))
+			table.insert(map.objects, obj)
+		end
+	end
+	xmlUnloadFile(xml)
+	return map
 end
 
 function MapLoader.create()
 	local self = setmetatable({}, MapLoader)
 
 	self.attrs = {
-		spawnpoint = {"position", "rotation", "vehicle", "paintjob", "upgrades"},
-		checkpoint = {"id", "nextid", "position", "size", "color", "type", "vehicle", "paintjob", "upgrades"},
-		object = {"position", "rotation", "model"},
-		pickup = {"posX", "posY", "posZ", "id", "type", "vehicle", "paintjob", "upgrades", "respawn"}
+		spawnpoint = {"posX", "posY", "posZ", "rotation", "vehicle", "upgrades", "dimension"},
+		checkpoint = {"id", "nextid", "posX", "posY", "posZ", "size", "color", "type", "vehicle", "upgrades", "dimension"},
+		object = {"posX", "posY", "posZ", "rotX", "rotY", "rotZ", "model", "dimension"},
+		pickup = {"posX", "posY", "posZ", "id", "type", "vehicle", "upgrades", "respawn", "dimension"}
 	}
 	self.maps = {}
 
 	return self
 end
 
-function MapLoader.load(map)
+function MapLoader:getMapData(map)
+	local result = {}
+	local childs = xmlNodeGetChildren(map)
+	for i,node in ipairs (childs) do
+		local type = self.attrs[xmlNodeGetName(node)]
+		table.insert(result,{})
+		for k, v in pairs(type) do
+			local attr = xmlNodeGetAttribute(node, v)
+			if (attr) then
+				table.insert(result[i],v,attr)
+			end
+		end
+		return result
+	end
 end
 
-function MapLoader:getByType(type)
+function MapLoader:getAllByType(type)
 	return MapLoader.constructors[type]
 end
 
-function MapLoader:register(type,map)
+function MapLoader:getByID(type,id)
+	for k, v in pairs(MapLoader.constructors[type]) do
+		if (k == id) then
+			return v
+		end
+	end
+end
+
+function MapLoader:register(type,name)
 	if (not MapLoader.constructors[type]) then
 		MapLoader.constructors[type] = {}
 	}
-	table.insert(MapLoader.constructors[type],("maps/%s/%s.map"):format(type,map))
+	table.insert(MapLoader.constructors[type],{name = name, src = ("maps/%s/%s.map"):format(type,name)})
 end
