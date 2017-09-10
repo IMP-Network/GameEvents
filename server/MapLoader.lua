b2ae905f6a6de19e078002eaba6d69b48e0020bd
Banex <mini-banex@hotmail.com>
2017-09-10 10:22:40 -0300

    Update MapLoader.lua

--- server/MapLoader.lua
+++ server/MapLoader.lua
@@ -1,10 +1,27 @@
 MapLoader = setmetatable({}, EventManager)
 MapLoader.__parent = EventManager
 MapLoader.instance = nil
+MapLoader.constructors = {}
 
-MapLoader.ATTRS = {
-	spawnpoint = { 'position', 'rotation', 'vehicle', 'paintjob', 'upgrades' },
-	checkpoint = { 'id', 'nextid', 'position', 'size', 'color', 'type', 'vehicle', 'paintjob', 'upgrades' },
-	object = { 'position', 'rotation', 'model' },
-	pickup = { 'posX', 'posY', 'posZ', 'id', 'type', 'vehicle', 'paintjob', 'upgrades', 'respawn' }
-}
+function MapLoader.getInstance()
+	if(not MapLoader.instance) then
+		MapLoader.instance = MapLoader.new():init()
+	end
+	return MapLoader.instance
+end
+
+function MapLoader.new()
+	local self = setmetatable({}, MapLoader)
+
+	self.attrs = {
+		spawnpoint = {"position", "rotation", "vehicle", "paintjob", "upgrades"},
+		checkpoint = {"id", "nextid", "position", "size", "color", "type", "vehicle", "paintjob", "upgrades"},
+		object = {"position", "rotation", "model"},
+		pickup = {"posX", "posY", "posZ", "id", "type", "vehicle", "paintjob", "upgrades", "respawn"}
+	}
+
+	return self
+end
+
+function MapLoader.load(map)
+end
